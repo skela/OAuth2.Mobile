6db084ca0247715c93d9e486ea59f358c84f58d3
Erik Schierboom <erik_schierboom@hotmail.com>
2013-03-30 11:41:38 +0100

    Fixed bugs in RestClientExtensions and added tests

--- src/OAuth2.Mobile/RestClientExtensions.cs
+++ src/OAuth2.Mobile/RestClientExtensions.cs
@@ -1,12 +1,12 @@
 ﻿namespace StudioDonder.OAuth2.Mobile
 {
     using System;
-    using System.Net;
     using System.Threading;
     using System.Threading.Tasks;
-    using System.Web;
 
     using RestSharp;
+
+    using Validation;
 
     /// <summary>
     /// Extensions to the <see cref="IRestClient"/> interface.
@@ -25,24 +25,22 @@
         /// </returns>
         public static Task<T> ExecuteAsync<T>(this IRestClient client, IRestRequest request, CancellationToken token)
         {
-            var taskCompletionSource = new TaskCompletionSource<T>();
+            Requires.NotNull(client, "client");
+            Requires.NotNull(request, "request");
             
+            var taskCompletionSource = new TaskCompletionSource<T>();
+
             try
             {
                 var async = client.ExecuteAsync<T>(request, (response, _) =>
                     {
-                        if (token.IsCancellationRequested || response == null)
+                        if (token.IsCancellationRequested)
                         {
-                            return;
+                            taskCompletionSource.TrySetCanceled();
                         }
-
-                        if (response.ErrorException != null)
+                        else if (response.ErrorException != null)
                         {
                             taskCompletionSource.TrySetException(response.ErrorException);
-                        }
-                        else if (IsHttpException(response.StatusCode))
-                        {
-                            taskCompletionSource.TrySetException(new HttpException((int)response.StatusCode, response.StatusDescription));
                         }
                         else
                         {
@@ -71,14 +69,43 @@
         /// <param name="request">The request.</param>
         /// <param name="token">The token.</param>
         /// <returns>The asynchronous request task.</returns>
-        public static Task ExecuteAsync(this IRestClient client, IRestRequest request, CancellationToken token)
+        public static Task<IRestResponse> ExecuteAsync(this IRestClient client, IRestRequest request, CancellationToken token)
         {
-            return client.ExecuteAsync<IRestResponse>(request, token);
-        }
+            Requires.NotNull(client, "client");
+            Requires.NotNull(request, "request");
 
-        private static bool IsHttpException(HttpStatusCode httpStatusCode)
-        {
-            return (int)httpStatusCode >= 400;
+            var taskCompletionSource = new TaskCompletionSource<IRestResponse>();
+
+            try
+            {
+                var async = client.ExecuteAsync(request, (response, _) =>
+                    {
+                        if (token.IsCancellationRequested)
+                        {
+                            taskCompletionSource.TrySetCanceled();
+                        }
+                        else if (response.ErrorException != null)
+                        {
+                            taskCompletionSource.TrySetException(response.ErrorException);
+                        }
+                        else
+                        {
+                            taskCompletionSource.TrySetResult(response);
+                        }
+                    });
+
+                token.Register(() =>
+                    {
+                        async.Abort();
+                        taskCompletionSource.TrySetCanceled();
+                    });
+            }
+            catch (Exception ex)
+            {
+                taskCompletionSource.TrySetException(ex);
+            }
+
+            return taskCompletionSource.Task;
         }
     }
 }
---
+++ tests/OAuth2.Mobile.Tests/RestClientExtensionsTests.cs
@@ -0,0 +1,159 @@
+﻿namespace StudioDonder.OAuth2.Mobile.Tests
+{
+    using System;
+    using System.Threading;
+
+    using RestSharp;
+
+    using Xunit;
+
+    public class RestClientExtensionsTests
+    {
+        [Fact]
+        public void ExecuteAsyncOnNullRestClientThrowsArgumentNullException()
+        {
+            // Arrange
+            IRestClient nullRestClient = null;
+
+            // Act
+
+            // Assert
+            Assert.Throws<ArgumentNullException>(() => nullRestClient.ExecuteAsync(CreateRestRequest(), CancellationToken.None));
+        }
+
+        [Fact]
+        public void ExecuteAsyncWithNullRestRequestThrowsArgumentNullException()
+        {
+            // Arrange
+            IRestRequest nullRestRequest = null;
+            var restClient = new RestClient();
+
+            // Act
+
+            // Assert
+            Assert.Throws<ArgumentNullException>(() => restClient.ExecuteAsync(nullRestRequest, CancellationToken.None));
+        }
+
+        [Fact]
+        public void ExecuteAsyncOnSuccessfulRequestReturnsRestResponseWithContentSet()
+        {
+            // Arrange
+            var restClient = CreateRestClient();
+
+            // Act
+            var executeAsyncTask = restClient.ExecuteAsync(CreateRestRequest(), CancellationToken.None);
+            executeAsyncTask.Wait();
+
+            // Assert
+            Assert.Equal("{\n   \"one\": \"two\",\n   \"key\": \"value\"\n}\n", executeAsyncTask.Result.Content);
+        }
+
+        [Fact]
+        public void ExecuteAsyncCorrectlyHandlesCancellation()
+        {
+            // Arrange
+            var restClient = CreateRestClient();
+            var cancellationTokenSource = new CancellationTokenSource();
+
+            // Act
+            var executeAsyncTask = restClient.ExecuteAsync(CreateRestRequest(), cancellationTokenSource.Token);
+            cancellationTokenSource.Cancel();
+
+            // Assert
+            Assert.True(executeAsyncTask.IsCanceled);
+        }
+
+        [Fact]
+        public void ExecuteAsyncCorrectlyHandlesErrors()
+        {
+            // Arrange
+            var restClient = CreateInvalidRestClient();
+
+            // Act
+            var executeAsyncTask = restClient.ExecuteAsync(CreateRestRequest(), CancellationToken.None);
+            executeAsyncTask.ContinueWith(t => t.Result);
+
+            // Assert
+            Assert.True(executeAsyncTask.IsFaulted);
+        }
+
+        [Fact]
+        public void ExecuteAsyncGenericOnNullRestClientThrowsArgumentNullException()
+        {
+            // Arrange
+            IRestClient nullRestClient = null;
+
+            // Act
+
+            // Assert
+            Assert.Throws<ArgumentNullException>(() => nullRestClient.ExecuteAsync<string>(CreateRestRequest(), CancellationToken.None));
+        }
+
+        [Fact]
+        public void ExecuteAsyncGenericWithNullRestRequestThrowsArgumentNullException()
+        {
+            // Arrange
+            IRestRequest nullRestRequest = null;
+            var restClient = CreateRestClient();
+
+            // Act
+
+            // Assert
+            Assert.Throws<ArgumentNullException>(() => restClient.ExecuteAsync<string>(nullRestRequest, CancellationToken.None));
+        }
+
+        [Fact]
+        public void ExecuteAsyncGenericOnSuccessfulRequestReturnsCorrectlyDeserializedResponse()
+        {
+            // Arrange
+            var restClient = CreateRestClient();
+
+            // Act
+            var executeAsyncTask = restClient.ExecuteAsync<JsonData>(CreateRestRequest(), CancellationToken.None);
+            executeAsyncTask.Wait();
+
+            // Assert
+            Assert.Equal("value", executeAsyncTask.Result.Key);
+        }
+
+        [Fact]
+        public void ExecuteAsyncGenericCorrectlyHandlesCancellation()
+        {
+            // Arrange
+            var restClient = CreateRestClient();
+            var cancellationTokenSource = new CancellationTokenSource();
+
+            // Act
+            var executeAsyncTask = restClient.ExecuteAsync<JsonData>(CreateRestRequest(), cancellationTokenSource.Token);
+            cancellationTokenSource.Cancel();
+
+            // Assert
+            Assert.True(executeAsyncTask.IsCanceled);
+        }
+
+        private static RestClient CreateInvalidRestClient()
+        {
+            return new RestClient("http://xxxxxxxxxxxxxxxxxxxx.xx");
+        }
+
+        private static RestClient CreateRestClient()
+        {
+            return new RestClient("http://echo.jsontest.com/key/value/one/two");
+        }
+
+        private static RestRequest CreateRestRequest()
+        {
+            return new RestRequest(new Uri("/", UriKind.Relative));
+        }
+
+        private class JsonData
+        {
+            public string Key { get; set; }
+        }
+
+        private class InvalidJsonData
+        {
+            public string Test { get; set; }
+        }
+    }
+}
